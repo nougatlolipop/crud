699291a4c4edd05386eed5014366ed49db219af7
nougatlolipop <august.ivan20@gmail.com>
2023-07-09 15:44:59 +0700

    first commit

---
+++ app/Views/sapa.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="<?= base_url() ?>assets/css/gaya.css">
+</head>
+
+<body>
+    <form method="post" action="/sapa">
+        <input type="text" name="nama" />
+        <button type="submit">Kirim</button>
+    </form>
+</body>
+
+</html>
